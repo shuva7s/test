fa7fccbeef9db0aaede94dce3f742713fc60dcf7
Shuvadeep Mandal <shuvadeepmandal5@gamil.com>
2024-09-15 20:17:45 +0530

    new hello

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import { fetchGreeting } from "@/lib/some.action";
 
 export default async function Home() {
-  const message = await fetchGreeting("Shuvadeep");
+  // const message = await fetchGreeting("Alex");
   return (
     <main className="min-h-screen flex justify-center items-center">
-      <h1 className="text-3xl font-semibold">{message}</h1>
+      {/* <h1 className="text-3xl font-semibold">{message}</h1> */}
+      <h1 className="text-3xl font-semibold">hi</h1>
     </main>
   );
 }
